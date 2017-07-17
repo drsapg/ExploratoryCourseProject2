6c0864f4c542dd6f96c60297452016242b582c7c
drsapg <drsapg@gmail.com>
2017-07-16 22:10:48 -0500

    Plot4 correction and plot5

--- plot5.R
+++ plot5.R
@@ -1,5 +1,5 @@
-# This code answer the fourt plot
-# Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008? 
+# This code answer the fifth plot
+# How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City 
 
 #Loading libraries
 library(lubridate)
@@ -18,14 +18,14 @@ NEI$type <- as.factor(NEI$type)
 NEI$Pollutant <- as.factor(NEI$Pollutant)
 
 # Select the SCC codes of coal related products in the SCC table
-coal <- filter(SCC,grepl(".[Co]al$",EI.Sector)) %>% select(SCC)
-coal <- coal[[1]]
+motor <- filter(SCC,grepl("^[Mm]obile",EI.Sector)) %>% select(SCC)
+motor <- motor[[1]]
 
 #grouping by year
-coalpm25 <- filter(NEI,SCC %in% coal) %>% group_by(year) %>% summarize(emission = sum(Emissions))
+motorpm25 <- filter(NEI,SCC %in% motor, fips=="24510") %>% group_by(year) %>% summarize(emission = sum(Emissions))
 
 #Plotting
-png("Plot4.png")
-plot(as.numeric(coalpm25$year),coalpm25$emission, type="l", xaxt = "n", xlab="Year", ylab="PM 2.5 Emissions (tons)", main="Coal Combustion PM 2.5 production")
+png("Plot5.png")
+plot(as.numeric(motorpm25$year),motorpm25$emission, type="l", xaxt = "n", xlab="Year", ylab="PM 2.5 Emissions (tons)", main="Mobile Combustion PM 2.5 production - Baltimore")
 axis(1,1:4,labels=c(1999,2002,2005,2008))
 dev.off()
