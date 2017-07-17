7b0ebc4b6320de04e9a04c31285a3d11548865f1
drsapg <drsapg@gmail.com>
2017-07-16 19:43:17 -0500

    Plot2

--- plot2.R
+++ plot2.R
@@ -20,24 +20,24 @@ NEI$type <- as.factor(NEI$type)
 NEI$Pollutant <- as.factor(NEI$Pollutant)
 
 # function to get the pm25 values
-totalpm25 <- function(x,y, fips = NULL)
+totalpm25 <- function(x,y, f = NULL)
 {
-        pm25 <- filter(x,year==y)
-        total <- (sum(pm25$Emissions))/10^3
+        pm25 <- filter(x,year==y & fips == f )
+        total <- (sum(pm25$Emissions))
         }
 
 #Creates numeric vector for each year
-total0 <- totalpm25(NEI,1999)
-total1 <- totalpm25(NEI,2002)
-total2 <- totalpm25(NEI,2005)
-total3 <- totalpm25(NEI,2008)
+total0 <- totalpm25(NEI,1999,"24510")
+total1 <- totalpm25(NEI,2002,"24510")
+total2 <- totalpm25(NEI,2005,"24510")
+total3 <- totalpm25(NEI,2008,"24510")
 
 # Setting the range
 rng <- range(total0,total1,total2,total3)
 
 #Plotting
-png("Plot1.png")
-plot(1999,total0,pch=20, ylim=c(rng[1]-2000,rng[2]+2000), xlim=c(1998,2008),ylab="Total Emissions (in tons x 10^3)", xlab="Year", main="Total PM 2.5 Emissions")
+png("Plot2.png")
+plot(1999,total0,pch=20, ylim=c(rng[1]-2000,rng[2]+2000), xlim=c(1998,2008),ylab="Total Emissions (in tons)", xlab="Year", main="Total PM 2.5 Emissions Baltimore")
 points(2002,total1,pch=20)
 points(2005,total2,pch=20)
 points(2008,total3,pch=20)
